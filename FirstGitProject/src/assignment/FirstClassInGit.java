31c52ef1b2c529af45df3b8f9d591b43c0b5a37d
am17125 <am17125>
2019-08-21 16:13:43 -0400

    done

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -5,6 +5,8 @@ public class FirstClassInGit {
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
 		
+		System.out.println("Austin Moats");
+		
 		//add a statement to display your name in the console
 		
 		//Then commit your changes
