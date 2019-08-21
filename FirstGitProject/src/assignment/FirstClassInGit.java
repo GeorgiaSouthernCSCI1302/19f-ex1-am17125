2152b92b0aeef12bf579a50d0923a556e9875c61
am17125 <am17125@CEIT2553214X007.ad.georgiasouthern.edu>
2019-08-21 16:45:04 -0400

    test commit

--- FirstGitProject/src/assignment/FirstClassInGit.java
+++ FirstGitProject/src/assignment/FirstClassInGit.java
@@ -1,11 +1,17 @@
 package assignment;
+import java.util.Scanner;
 
 public class FirstClassInGit {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Hello my name is austin!");
 		
-		System.out.println("Austin Moats");
+		System.out.println("Please type your last name.");
+		
+		Scanner a = new Scanner(System.in);
+		
+		String b = a.next();
+		System.out.println(b);
 		
 		
 		//add a statement to display your name in the console
